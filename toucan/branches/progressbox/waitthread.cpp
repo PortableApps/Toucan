104261a80886e28695f686373893f0e2d535a970
Steven Lamerton <steve.lamerton@gmail.com>
2008-10-12 16:43:48 +0100

    [svn r213] Start of new progress box branch

---
+++ toucan/branches/progressbox/waitthread.cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Author:      Steven Lamerton
+// Copyright:   Copyright (C) 2008 Steven Lamerton
+// Licence:     GNU GPL 2 (See readme for more info
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "waitthread.h"
+//#include "backupprocess.h"
+
+void *WaitThread::Entry(){
+	HANDLE hProcess=OpenProcess(PROCESS_QUERY_INFORMATION,FALSE,m_PID);
+	DWORD lgReturn;
+	GetExitCodeProcess(hProcess, &lgReturn);
+	while(lgReturn == STILL_ACTIVE){
+		m_Process->HasInput();
+		GetExitCodeProcess(hProcess, &lgReturn);
+	}
+	return NULL;
+}
