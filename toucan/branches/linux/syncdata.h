3ff98a4863cfb6e16f5e8fe5d4e2946763d23e82
Steven Lamerton <steve.lamerton@gmail.com>
2008-10-28 21:17:01 +0000

    [svn r225] Cleaned up the messed up commit

--- toucan/branches/linux/syncdata.h
+++ toucan/branches/linux/syncdata.h
@@ -1,4 +1,3 @@
-<<<<<<< .working
 /////////////////////////////////////////////////////////////////////////////////
 // Author:      Steven Lamerton
 // Copyright:   Copyright (C) 2007-2008 Steven Lamerton
@@ -66,72 +65,4 @@ private:
 };
 
 #endif
-=======
-/////////////////////////////////////////////////////////////////////////////////
-// Author:      Steven Lamerton
-// Copyright:   Copyright (C) 2007-2008 Steven Lamerton
-// License:     GNU GPL 2 (See readme for more info)
-/////////////////////////////////////////////////////////////////////////////////
 
-#ifndef H_SYNCDATA
-#define H_SYNCDATA
-
-#include "frmmain.h"
-#include "rootdata.h"
-#include <wx\string.h>
-
-
-class SyncData : public RootData{
-
-public:
-
-	//Functions
-	bool TransferToFile();
-	bool TransferFromFile();
-	bool TransferToForm();
-	bool TransferFromForm();
-	
-	void Output();
-	
-	bool Execute(Rules rules);
-	
-	bool NeedsPassword() { return false; }
-	
-	bool NeededFieldsFilled();
-
-	//Inline functions
-	void SetSource(wxString source) { strSource = source; }
-	wxString GetSource() { return strSource; }
-
-	void SetDest(wxString dest) { strDest = dest; }
-	wxString GetDest() { return strDest; }
-
-	void SetFunction(wxString function) { strFunction = function; }
-	wxString GetFunction() { return strFunction; }
-
-	void SetTimeStamps(bool timestamps) { blTimeStamps = timestamps; }
-	bool GetTimeStamps() { return blTimeStamps; }
-
-	void SetAttributes(bool attributes) { blAttributes = attributes; }
-	bool GetAttributes() { return blAttributes; }
-
-	void SetIgnoreRO(bool ignorero) { blIgnoreRO = ignorero; }
-	bool  GetIgnoreRO() { return blIgnoreRO; }
-
-	void SetIgnoreDLS(bool ignoredls) { blIgnoreDLS = ignoredls; }
-	bool  GetIgnoreDLS() { return blIgnoreDLS; }
-	
-
-private:
-	wxString strSource;
-	wxString strDest;
-	wxString strFunction;
-	bool blTimeStamps;
-	bool blAttributes;
-	bool blIgnoreRO;
-	bool blIgnoreDLS;
-
-};
-
-#endif
->>>>>>> .merge-right.r223
