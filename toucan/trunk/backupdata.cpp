be1cd2f3052d56e21f4dca5bc3260fe038e43088
Steven Lamerton <steve.lamerton@gmail.com>
2009-01-22 20:53:21 +0000

    [svn r319] Fix individual file backup

--- toucan/trunk/backupdata.cpp
+++ toucan/trunk/backupdata.cpp
@@ -207,42 +207,57 @@ bool BackupData::CreateList(wxTextFile *file, Rules rules, wxString strPath, int
 	if(wxGetApp().ShouldAbort()){
 		return true;
 	}
-	//Clean up the path passed
-	if (strPath[strPath.length()-1] != wxFILE_SEP_PATH) {
-		strPath += wxFILE_SEP_PATH;       
-	}
 	wxGetApp().Yield();
-	wxDir dir(strPath);
-	wxString strFilename;
-	bool blDir = dir.GetFirst(&strFilename);
-	//If the path is ok
-	if(blDir){
-		//Loop through all of the files and folders in the directory
-		do {
-			//If it is a directory
-			if(wxDirExists(strPath + strFilename))
-			{
-				//Always call the function again to ensure that ALL files and folders are processed
-				CreateList(file, rules, strPath + strFilename, iRootLength);
-			}
-			//If it is a file
-			else{
-				if(!rules.ShouldExclude(strPath + strFilename, false)){
-					if(iRootLength == 0){
-						wxString strCombined = strPath + strFilename;
-						strCombined = strCombined.Right(strCombined.Length() - iRootLength - 3);
-						file->AddLine(strCombined);
-					}
-					else{
-						wxString strCombined = strPath + strFilename;
-						strCombined = strCombined.Right(strCombined.Length() - iRootLength - 1);
-						file->AddLine(strCombined);
+	if(wxDirExists(strPath)){
+		//Clean up the path passed
+		if (strPath[strPath.length()-1] != wxFILE_SEP_PATH) {
+			strPath += wxFILE_SEP_PATH;       
+		}
+		wxDir dir(strPath);
+		wxString strFilename;
+		bool blDir = dir.GetFirst(&strFilename);
+		//If the path is ok
+		if(blDir){
+			//Loop through all of the files and folders in the directory
+			do {
+				//If it is a directory
+				if(wxDirExists(strPath + strFilename))
+				{
+					//Always call the function again to ensure that ALL files and folders are processed
+					CreateList(file, rules, strPath + strFilename, iRootLength);
+				}
+				//If it is a file
+				else{
+					if(!rules.ShouldExclude(strPath + strFilename, false)){
+						if(iRootLength == 0){
+							wxString strCombined = strPath + strFilename;
+							strCombined = strCombined.Right(strCombined.Length() - iRootLength - 3);
+							file->AddLine(strCombined);
+						}
+						else{
+							wxString strCombined = strPath + strFilename;
+							strCombined = strCombined.Right(strCombined.Length() - iRootLength - 1);
+							file->AddLine(strCombined);
+						}
 					}
 				}
 			}
-		}
-		while (dir.GetNext(&strFilename) );
-	}  
+			while (dir.GetNext(&strFilename) );
+		}  
+	}
+	//We have been passed a file
+	else{
+		if(!rules.ShouldExclude(strPath, false)){
+			if(iRootLength == 0){
+				strPath = strPath.Right(strPath.Length() - iRootLength - 3);
+				file->AddLine(strPath);
+			}
+			else{
+				strPath = strPath.Right(strPath.Length() - iRootLength - 1);
+				file->AddLine(strPath);
+			}
+		}		
+	}
 	return true;
 }
 
