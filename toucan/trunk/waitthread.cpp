111e5c12faf2bd59fbcc9226800abd32f20c3833
Steven Lamerton <steve.lamerton@gmail.com>
2009-01-26 17:15:39 +0000

    [svn r322] Removed msw specific code in waitthread

--- toucan/trunk/waitthread.cpp
+++ toucan/trunk/waitthread.cpp
@@ -5,17 +5,10 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 #include "waitthread.h"
-//#include "backupprocess.h"
 
 void *WaitThread::Entry(){
-	#ifdef __WXMSW__
-		HANDLE hProcess=OpenProcess(PROCESS_QUERY_INFORMATION,FALSE,m_PID);
-		DWORD lgReturn;
-		GetExitCodeProcess(hProcess, &lgReturn);
-		while(lgReturn == STILL_ACTIVE){
-			m_Process->HasInput();
-			GetExitCodeProcess(hProcess, &lgReturn);
-		}
-	#endif
+	while(wxProcess::Exists(m_PID)){
+		m_Process->HasInput();
+	}
 	return NULL;
 }
