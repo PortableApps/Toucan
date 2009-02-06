376b384c50e551a939f4d22b3f9d71cc6351a94a
Steven Lamerton <steve.lamerton@gmail.com>
2009-02-06 10:06:20 +0000

    [svn r336] Added label to insert variable box

--- toucan/trunk/frmvariable.cpp
+++ toucan/trunk/frmvariable.cpp
@@ -46,49 +46,49 @@ void frmVariable::CreateControls()
     wxBoxSizer* TopSizer = new wxBoxSizer(wxVERTICAL);
     Dialog->SetSizer(TopSizer);
 
-		wxBoxSizer* LocationSizer = new wxBoxSizer(wxHORIZONTAL);
-		TopSizer->Add(LocationSizer, 0, wxGROW|wxALL, 5);
+	wxBoxSizer* LocationSizer = new wxBoxSizer(wxHORIZONTAL);
+	TopSizer->Add(LocationSizer, 0, wxGROW|wxALL, 5);
 		
-			wxStaticText* LocationStaticText = new wxStaticText(Dialog, wxID_STATIC, _("Location"));
-			LocationSizer->Add(LocationStaticText, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxStaticText* LocationStaticText = new wxStaticText(Dialog, wxID_STATIC, _("Location"));
+	LocationSizer->Add(LocationStaticText, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 			
-			m_Location_Text = new wxTextCtrl(Dialog, ID_LOCATION_TEXT, _T(""), wxDefaultPosition, wxSize(-1, -1));
-			m_Location_Text->SetMinSize(wxSize(300, -1));
-			LocationSizer->Add(m_Location_Text, 1, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	m_Location_Text = new wxTextCtrl(Dialog, ID_LOCATION_TEXT, _T(""), wxDefaultPosition, wxSize(-1, -1));
+	m_Location_Text->SetMinSize(wxSize(300, -1));
+	LocationSizer->Add(m_Location_Text, 1, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-			wxButton* LocationButton = new wxButton(Dialog, ID_LOCATION, wxT("..."), wxDefaultPosition, wxSize(25, -1), 0);
-			LocationSizer->Add(LocationButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxButton* LocationButton = new wxButton(Dialog, ID_LOCATION, wxT("..."), wxDefaultPosition, wxSize(25, -1), 0);
+	LocationSizer->Add(LocationButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-		wxBoxSizer* VariablesSizer = new wxBoxSizer(wxHORIZONTAL);
-		TopSizer->Add(VariablesSizer, 0, wxALIGN_LEFT|wxALL, 5);
+	wxBoxSizer* VariablesSizer = new wxBoxSizer(wxHORIZONTAL);
+	TopSizer->Add(VariablesSizer, 0, wxALIGN_LEFT|wxALL, 5);
 		
-			wxStaticText* VariablesStaticText = new wxStaticText(Dialog, wxID_STATIC, _("Portable Variables"), wxDefaultPosition, wxDefaultSize, 0);
-			VariablesSizer->Add(VariablesStaticText, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxStaticText* VariablesStaticText = new wxStaticText(Dialog, wxID_STATIC, _("Variables"), wxDefaultPosition, wxDefaultSize, 0);
+	VariablesSizer->Add(VariablesStaticText, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-			wxArrayString m_VariablesStrings;
-			m_Variables_Combo = new wxComboBox(Dialog, ID_VARIABLES_COMBO, wxEmptyString, wxDefaultPosition, wxDefaultSize, m_VariablesStrings, 0);
-			VariablesSizer->Add(m_Variables_Combo, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxArrayString m_VariablesStrings;
+	m_Variables_Combo = new wxComboBox(Dialog, ID_VARIABLES_COMBO, wxEmptyString, wxDefaultPosition, wxDefaultSize, m_VariablesStrings, 0);
+	VariablesSizer->Add(m_Variables_Combo, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-			wxBitmapButton* VariablesButton = new wxBitmapButton(Dialog, ID_VARIABLES, this->GetBitmapResource(wxT("add.png")), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW);
-			VariablesSizer->Add(VariablesButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxBitmapButton* VariablesButton = new wxBitmapButton(Dialog, ID_VARIABLES, this->GetBitmapResource(wxT("add.png")), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW);
+	VariablesSizer->Add(VariablesButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-		wxBoxSizer* PreviewSizer = new wxBoxSizer(wxHORIZONTAL);
-		TopSizer->Add(PreviewSizer, 0, wxGROW|wxALL, 5);
+	wxBoxSizer* PreviewSizer = new wxBoxSizer(wxHORIZONTAL);
+	TopSizer->Add(PreviewSizer, 0, wxGROW|wxALL, 5);
 
-			wxStaticText* PreviewStaticText = new wxStaticText(Dialog, wxID_STATIC, _("Preview"));
-			PreviewSizer->Add(PreviewStaticText, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxStaticText* PreviewStaticText = new wxStaticText(Dialog, wxID_STATIC, _("Preview"));
+	PreviewSizer->Add(PreviewStaticText, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-			m_Preview_Text = new wxTextCtrl(Dialog, ID_PREVIEW_TEXT, _T(""), wxDefaultPosition, wxDefaultSize, wxTE_READONLY);
-			PreviewSizer->Add(m_Preview_Text, 1, wxGROW|wxALL, 5);
+	m_Preview_Text = new wxTextCtrl(Dialog, ID_PREVIEW_TEXT, _T(""), wxDefaultPosition, wxDefaultSize, wxTE_READONLY);
+	PreviewSizer->Add(m_Preview_Text, 1, wxGROW|wxALL, 5);
 
-		wxBoxSizer* ButtonSizer = new wxBoxSizer(wxHORIZONTAL);
-		TopSizer->Add(ButtonSizer, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
+	wxBoxSizer* ButtonSizer = new wxBoxSizer(wxHORIZONTAL);
+	TopSizer->Add(ButtonSizer, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
 
-			wxButton* OKButton = new wxButton(Dialog, wxID_OK, _("OK"));
-			ButtonSizer->Add(OKButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxButton* OKButton = new wxButton(Dialog, wxID_OK, _("OK"));
+	ButtonSizer->Add(OKButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-			wxButton* CancelButton = new wxButton(Dialog, wxID_CANCEL, _("Cancel"));
-			ButtonSizer->Add(CancelButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+	wxButton* CancelButton = new wxButton(Dialog, wxID_CANCEL, _("Cancel"));
+	ButtonSizer->Add(CancelButton, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 		
 	wxString strPath = wxPathOnly(wxStandardPaths::Get().GetExecutablePath()) + wxFILE_SEP_PATH;
 	this->SetIcon(wxIcon(strPath + wxT("Toucan.ico"), wxBITMAP_TYPE_ICO));
@@ -99,6 +99,7 @@ void frmVariable::CreateControls()
 	m_Variables_Combo->Append(wxT("docs"));
 	m_Variables_Combo->Append(wxT("drive"));
 	m_Variables_Combo->Append(wxT("volume"));
+	m_Variables_Combo->Append(wxT("label"));
 	m_Variables_Combo->Append(wxT("YYYY"));
 	m_Variables_Combo->Append(wxT("MM"));
 	m_Variables_Combo->Append(wxT("DD"));
