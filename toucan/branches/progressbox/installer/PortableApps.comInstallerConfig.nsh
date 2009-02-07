453bba44644b23e7707db050010801914d30df08
Steven Lamerton <steve.lamerton@gmail.com>
2009-02-07 13:37:37 +0000

    [svn r345] New progressbox branch

---
+++ toucan/branches/progressbox/installer/PortableApps.comInstallerConfig.nsh
@@ -0,0 +1,182 @@
+;Copyright 2007-2009 John T. Haller of PortableApps.com
+;Website: http://PortableApps.com/
+
+;This software is OSI Certified Open Source Software.
+;OSI Certified is a certification mark of the Open Source Initiative.
+
+;This program is free software; you can redistribute it and/or
+;modify it under the terms of the GNU General Public License
+;as published by the Free Software Foundation; either version 2
+;of the License, or (at your option) any later version.
+
+;This program is distributed in the hope that it will be useful,
+;but WITHOUT ANY WARRANTY; without even the implied warranty of
+;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;GNU General Public License for more details.
+
+;You should have received a copy of the GNU General Public License
+;along with this program; if not, write to the Free Software
+;Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+;EXCEPTION: Can be used with non-GPL apps distributed by PortableApps.com
+
+;OVERVIEW: This file is used to hold configuration data for the PortableApps.comInstaller
+;It should be placed in the Other\Source directory along with PortableApps.comInstaller.nsi.
+;All configuration data is contained within it.  If any custom pre-install, post-install or 
+;cleanup when the optional features are not selected is needed, it can be included in the
+;PortableApps.comInstallerCustom.nsi which is detailed within that file.
+
+;LAST UPDATED FOR INSTALLER VERSION: 0.11.1
+
+;== Basic Information.  Basic information about the portable app
+!define NAME "Toucan"
+!define SHORTNAME "Toucan"
+!define VERSION "2.0.4.0"
+!define FILENAME "Toucan_2.0.4"
+!define CHECKRUNNING "Toucan.exe"
+!define CLOSENAME "Toucan"
+!define ADDONSDIRECTORYPRESERVE "NONE" ;use "NONE" for no plugins directory or relative to install directory: App\AppName\plugins
+!define INSTALLERCOMMENTS "For additional details, visit PortableApps.com"
+!define INSTALLERADDITIONALTRADEMARKS "" ;end this entry with a period and a space if used
+!define INSTALLERMULTILINGUAL "true" ;uncomment this entry for multilingual installers
+
+!ifndef INSTALLERMULTILINGUAL
+	;== Single language installer
+	!define INSTALLERLANGUAGE "English" ;one of the NSIS pre-defined languages supported: English, French, Italian, German, Japanese, SimpChinese
+!else
+	;== Multilingual installer, uncomment the lanuages used
+	!define USES_ENGLISH "true"
+	;!define USES_AFRIKAANS "true"
+	;!define USES_ALBANIAN "true"
+	;!define USES_ARABIC "true"
+	;!define USES_BASQUE "true"
+	;!define USES_BELARUSIAN "true"
+	;!define USES_BOSNIAN "true"
+	;!define USES_BRETON "true"
+	!define USES_BULGARIAN "true"
+	;!define USES_CATALAN "true"
+	;!define USES_CROATIAN "true"
+	;!define USES_CZECH "true"
+	;!define USES_DANISH "true"
+	!define USES_DUTCH "true"
+	!define USES_ESTONIAN "true"
+	;!define USES_FARSI "true"
+	;!define USES_FINNISH "true"
+	!define USES_FRENCH "true"
+	;!define USES_GALICIAN "true"
+	!define USES_GERMAN "true"
+	;!define USES_GREEK "true"
+	;!define USES_HEBREW "true"
+	!define USES_HUNGARIAN "true"
+	;!define USES_ICELANDIC "true"
+	;!define USES_INDONESIAN "true"
+	;!define USES_IRISH "true"
+	!define USES_ITALIAN "true"
+	;!define USES_JAPANESE "true"
+	!define USES_KOREAN "true"
+	;!define USES_KURDISH "true"
+	;!define USES_LATVIAN "true"
+	;!define USES_LITHUANIAN "true"
+	;!define USES_LUXEMBOURGISH "true"
+	;!define USES_MACEDONIAN "true"
+	;!define USES_MALAY "true"
+	;!define USES_MONGOLIAN "true"
+	;!define USES_NORWEGIAN "true"
+	;!define USES_NORWEGIANNYNORSK "true"
+	!define USES_POLISH "true"
+	;!define USES_PORTUGUESE "true"
+	;!define USES_PORTUGUESEBR "true"
+	;!define USES_ROMANIAN "true"
+	!define USES_RUSSIAN "true"
+	;!define USES_SERBIAN "true"
+	;!define USES_SERBIANLATIN "true"
+	;!define USES_SIMPCHINESE "true"
+	;!define USES_SLOVAK "true"
+	;!define USES_SLOVENIAN "true"
+	!define USES_SPANISH "true"
+	;!define USES_SPANISHINTERNATIONAL "true"
+	;!define USES_SWEDISH "true"
+	;!define USES_THAI "true"
+	;!define USES_TRADCHINESE "true"
+	;!define USES_TURKISH "true"
+	;!define USES_UKRANIAN "true"
+	;!define USES_UZBECK "true"
+	;!define USES_WELSH "true"
+!endif
+
+;== License.  For no license agreement, comment out the next line by placing a semicolon at the start of it
+;!define LICENSEAGREEMENT "eula.rtf"
+
+;== Multi-Installer.  If making an installer with no options (like additional languages), comment out the next line by placing a semicolon at the start of it
+;!define MAINSECTIONTITLE "AppName Portable (English) [Required]"
+!ifdef MAINSECTIONTITLE
+	!define MAINSECTIONDESCRIPTION "Install the portable app"
+	!define OPTIONALSECTIONTITLE "Additional Languages"
+	!define OPTIONALSECTIONDESCRIPTION "Add multilingual support for this app"
+	!define OPTIONALSECTIONSELECTEDAPPINFOSUFFIX "(Multilingual)"
+	!define OPTIONALSECTIONNOTSELECTEDAPPINFOSUFFIX "(English)"
+!endif
+
+;== Plugin Installer.  If this is a plugin installer (not a full app installer), uncomment this line to skip adding the app size to the free space calculation and files in root directory
+;!define PLUGININSTALLER "true"
+
+;== Remove Main Directories.  If you want to remove one of the main directories before installing (useful with upgrades) uncomment the appropriate line.
+;!define REMOVEAPPDIRECTORY "true"
+;!define REMOVEDATADIRECTORY "true" ;Normally this should not be used
+;!define REMOVEOTHERDIRECTORY "true"
+
+;== Preserve Specific Directories.  If you are removing directories but wish to preserve a specific directory, define them here.
+;!define PRESERVEDIRECTORY1 "App\Example\Plugins"
+;!define PRESERVEDIRECTORY2 ""
+;!define PRESERVEDIRECTORY3 ""
+;!define PRESERVEDIRECTORY4 ""
+;!define PRESERVEDIRECTORY5 ""
+;!define PRESERVEDIRECTORY6 ""
+;!define PRESERVEDIRECTORY7 ""
+;!define PRESERVEDIRECTORY8 ""
+;!define PRESERVEDIRECTORY9 ""
+;!define PRESERVEDIRECTORY10 ""
+
+;== Preserve Specific Files.  If you are removing directories but wish to preserve a specific file, define them here.  Wildcards work.
+;!define PRESERVEFILE1 "App\Example\Plugin.dll"
+;!define PRESERVEFILE2 ""
+;!define PRESERVEFILE3 ""
+;!define PRESERVEFILE4 ""
+;!define PRESERVEFILE5 ""
+;!define PRESERVEFILE6 ""
+;!define PRESERVEFILE7 ""
+;!define PRESERVEFILE8 ""
+;!define PRESERVEFILE9 ""
+;!define PRESERVEFILE10 ""
+
+;== Remove Specific Directories.  If you wish to remove specific directories and all files within, define them here.
+;!define REMOVEDIRECTORY1 "App\Example"
+;!define REMOVEDIRECTORY2 ""
+;!define REMOVEDIRECTORY3 ""
+;!define REMOVEDIRECTORY4 ""
+;!define REMOVEDIRECTORY5 ""
+;!define REMOVEDIRECTORY6 ""
+;!define REMOVEDIRECTORY7 ""
+;!define REMOVEDIRECTORY8 ""
+;!define REMOVEDIRECTORY9 ""
+;!define REMOVEDIRECTORY10 ""
+
+;== Remove Specific Files. If you wish to remove specific files, define them here.  Wildcards are acceptable: App\Example\Example.*
+;!define REMOVEFILE1 "App\Example\example.exe"
+;!define REMOVEFILE2 ""
+;!define REMOVEFILE3 ""
+;!define REMOVEFILE4 ""
+;!define REMOVEFILE5 ""
+;!define REMOVEFILE6 ""
+;!define REMOVEFILE7 ""
+;!define REMOVEFILE8 ""
+;!define REMOVEFILE9 ""
+;!define REMOVEFILE10 ""
+
+;== Custom Code.  If you need custom code pre or post install or to cleanup when optional features are not selected, uncomment the appropriate lines here and include it in the PortableApps.comInstallerCustom.nsi file.
+!define USESCUSTOMCODE "true"
+;!define USESCONFIGREAD "true" ;Uncomment this line if you need to use ConfigRead functions in your custom code
+;!define USESCONFIGREADS "true" ;Uncomment this line if you need to use ConfigReadS functions in your custom code
+;!define USESCONFIGWRITE "true" ;Uncomment this line if you need to use ConfigWrite functions in your custom code
+;!define USESCONFIGWRITES "true" ;Uncomment this line if you need to use ConfigWriteS functions in your custom code
+;!define USESVERSIONCOMPARE "true" ;Uncomment this line if you need to use VersionCompare functions in your custom code
---
+++ toucan/branches/progressbox/sync.cpp
@@ -0,0 +1,370 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Author:      Steven Lamerton
+// Copyright:   Copyright (C) 2006-2008 Steven Lamerton
+// License:     GNU GPL 2 (See readme for more info)
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "sync.h"
+#include "basicfunctions.h"
+#include "frmprogress.h"
+#include "frmmain.h"
+#include "toucan.h"
+#include "variables.h"
+
+//Include the hashlib++ library for MD5 support
+#include <hashlibpp.h>
+
+#include <wx/dir.h>
+
+void *SyncThread::Entry(){
+	//Launch the correct set of loops
+	if(m_Data.GetFunction() == _("Copy") || m_Data.GetFunction() == _("Update")){
+		SyncLoop(m_Data, m_Rules);
+		if(m_Data.GetTimeStamps()){
+			FolderTimeLoop(m_Data.GetSource(), m_Data.GetDest());		
+		}
+	}
+	else if(m_Data.GetFunction() ==  _("Mirror (Copy)")){
+		m_Data.SetFunction(_("Copy"));
+		SyncLoop(m_Data, m_Rules);
+		if(m_Data.GetTimeStamps()){
+			FolderTimeLoop(m_Data.GetSource(), m_Data.GetDest());
+		}
+		//Swap the source and dest around for the mirror routine
+		wxString strTemp = m_Data.GetSource();
+		m_Data.SetSource(m_Data.GetDest());
+		m_Data.SetDest(strTemp);
+		//Run the mirror loop
+		m_Data.SetFunction(_("Mirror (Copy)"));
+		SyncLoop(m_Data, m_Rules);
+		if(m_Data.GetTimeStamps()){
+			FolderTimeLoop(m_Data.GetSource(), m_Data.GetDest());
+		}
+	}
+	else if(m_Data.GetFunction() ==  _("Mirror (Update)")){
+		m_Data.SetFunction(_("Update"));
+		SyncLoop(m_Data, m_Rules);
+		if(m_Data.GetTimeStamps()){
+			FolderTimeLoop(m_Data.GetSource(), m_Data.GetDest());
+		}
+		//Swap the source and dest around for the mirror routine
+		wxString strTemp = m_Data.GetSource();
+		m_Data.SetSource(m_Data.GetDest());
+		m_Data.SetDest(strTemp);
+		//Run the mirror loop
+		m_Data.SetFunction(_("Mirror (Update)"));
+		SyncLoop(m_Data, m_Rules);
+		if(m_Data.GetTimeStamps()){
+			FolderTimeLoop(m_Data.GetSource(), m_Data.GetDest());
+		}
+	}
+	else if(m_Data.GetFunction() ==  _("Equalise")){
+		m_Data.SetFunction(_("Update"));
+		SyncLoop(m_Data, m_Rules);
+		if(m_Data.GetTimeStamps()){
+			FolderTimeLoop(m_Data.GetSource(), m_Data.GetDest());
+		}
+		wxString strTemp = m_Data.GetSource();
+		m_Data.SetSource(m_Data.GetDest());
+		m_Data.SetDest(strTemp);
+		m_Data.SetFunction(_("Update"));
+		SyncLoop(m_Data, m_Rules);
+		if(m_Data.GetTimeStamps()){
+			FolderTimeLoop(m_Data.GetSource(), m_Data.GetDest());
+		}
+	}
+	if(wxGetApp().blGUI){
+		wxMutexGuiEnter();
+		m_Main->m_Sync_Source_Tree->DeleteAllItems();
+		m_Main->m_Sync_Dest_Tree->DeleteAllItems();
+		
+		m_Main->m_Sync_Source_Tree->AddRoot(_("Hidden text"));
+		m_Main->m_Sync_Dest_Tree->AddRoot(_("Hidden text"));
+		
+		m_Main->m_Sync_Source_Tree->AddNewPath(Normalise(Normalise(m_Main->m_Sync_Source_Txt->GetValue())));
+		m_Main->m_Sync_Dest_Tree->AddNewPath(Normalise(Normalise(m_Main->m_Sync_Dest_Txt->GetValue())));		
+		wxMutexGuiLeave();
+	}
+	wxCommandEvent event(wxEVT_COMMAND_BUTTON_CLICKED, ID_SCRIPTFINISH);
+	wxPostEvent(wxGetApp().MainWindow, event);
+	//As we are finished cancel any aborts
+	wxGetApp().SetAbort(false);
+	return NULL;
+
+}
+
+bool SyncLoop(SyncData data, Rules rules)
+{
+	if(wxGetApp().ShouldAbort()){
+		return true;
+	}
+	wxString strTo = data.GetDest();
+	wxString strFrom = data.GetSource();
+	//Append a slash in case of an incorrect pass
+	if (strTo[strTo.length()-1] != wxFILE_SEP_PATH) {
+		strTo += wxFILE_SEP_PATH;       
+	}
+	// for both dirs
+	if (strFrom[strFrom.length()-1] != wxFILE_SEP_PATH) {
+		strFrom += wxFILE_SEP_PATH;       
+	}
+	if (!wxDirExists(strTo)){
+		if(!rules.ShouldExclude(strTo, true)){
+			wxMkdir(strTo);
+		}
+		else{
+			return false;
+		}
+	}
+	wxDir dir(strFrom);
+	wxString strFilename;
+	bool blDir = dir.GetFirst(&strFilename);
+	if(blDir){
+		do {
+			if(wxGetApp().ShouldAbort()){
+				return true;
+			}
+			if(wxDirExists(strFrom + strFilename))
+			{
+				if(!rules.ShouldExclude(strTo + strFilename, true))
+				{ 
+					if (wxDirExists(strTo + strFilename)){
+						data.SetSource(strFrom + strFilename);
+						data.SetDest(strTo + strFilename);
+						SyncLoop(data, rules);
+					}
+					else{
+						//wxMessageBox(_("Doesnt exists"));
+						if(data.GetFunction() == _("Mirror (Copy)") || data.GetFunction() == _("Mirror (Update)")){
+							DirectoryRemove(strFrom + strFilename);
+						}
+						else{
+							wxMkdir(strTo + strFilename);
+							data.SetSource(strFrom + strFilename);
+							data.SetDest(strTo + strFilename);
+							SyncLoop(data, rules);
+						}
+					}
+				}
+			}
+			else{
+				data.SetSource(strFrom + strFilename);
+				data.SetDest(strTo + strFilename);
+				SyncFile(data, rules);
+			}
+		}
+		while (dir.GetNext(&strFilename) );
+	} 
+	return true;
+	
+}
+
+bool SyncFile(SyncData data, Rules rules)
+{
+	if(wxGetApp().ShouldAbort()){
+		return true;
+	}
+	bool ShouldTimeStamp = false;
+	#ifdef __WXMSW__
+		long iAttributes = 0;
+	#endif
+	if(!rules.ShouldExclude(data.GetDest(), false)){
+		if(data.GetIgnoreRO()){
+			#ifdef __WXMSW__
+				iAttributes = GetFileAttributes(data.GetDest());
+				if(iAttributes == -1){
+					iAttributes = FILE_ATTRIBUTE_NORMAL;					
+				}
+				SetFileAttributes(data.GetDest(),FILE_ATTRIBUTE_NORMAL); 
+			#endif
+		} 
+		if(data.GetFunction() == _("Copy")){	
+			if(wxCopyFile(data.GetSource(), wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"), true)){
+				if(wxRenameFile(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"), data.GetDest(), true)){
+					OutputProgress(data.GetSource() + _("\t copied to \t") + data.GetDest());
+					ShouldTimeStamp = true;
+				}
+			}
+			if(wxFileExists(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"))){
+				wxRemoveFile(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"));
+			}
+		}	
+		if(data.GetFunction() == _("Update")){
+			/*Check to see if the desination file exists, if it does then a time check is made, if not then 
+			the source file is always copied*/
+			if(wxFileExists(data.GetDest())){	
+				wxDateTime tmTo, tmFrom;
+				wxFileName flTo(data.GetDest());
+				wxFileName flFrom(data.GetSource());
+				
+				flTo.GetTimes(NULL, &tmTo, NULL);
+				flFrom.GetTimes(NULL, &tmFrom, NULL);		
+				
+				if(data.GetIgnoreDLS()){
+					tmFrom.MakeTimezone(wxDateTime::Local, true);
+				}
+				//Check to see if the source file is newer than the destination file, subtracts one hour to make up for timestamp errors
+				if(tmFrom.IsLaterThan(tmTo)){
+					hashwrapper *myWrapper = new md5wrapper();
+					std::string sourceHash = myWrapper->getHashFromFile(std::string(data.GetSource().mb_str()));
+					std::string desinationHash = myWrapper->getHashFromFile(std::string(data.GetDest().mb_str()));
+					delete myWrapper;
+					if(sourceHash != desinationHash){
+						if(wxCopyFile(data.GetSource(), wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"), true)){
+							if(wxRenameFile(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"), data.GetDest(), true)){
+								OutputProgress(data.GetSource() + _("\t updated \t") + data.GetDest());
+								ShouldTimeStamp = true;
+							}
+						}
+						if(wxFileExists(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"))){
+							wxRemoveFile(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"));
+						}
+					}
+				}
+			}
+			else{
+				if(wxCopyFile(data.GetSource(), wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"), true)){
+					if(wxRenameFile(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"), data.GetDest(), true)){
+						OutputProgress(data.GetSource() + _("\t copied to \t") + data.GetDest());
+						ShouldTimeStamp = true;
+					}
+				}
+				if(wxFileExists(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"))){
+					wxRemoveFile(wxPathOnly(data.GetDest()) + wxFILE_SEP_PATH + wxT("Toucan.tmp"));
+				}
+			}
+		}
+		if(data.GetFunction() == _("Mirror (Copy)") || data.GetFunction() == _("Mirror (Update)")){	
+			if(!wxFileExists(data.GetDest())){
+				wxRemoveFile(data.GetSource());
+				OutputProgress(_("Removed ") + data.GetSource());
+			}
+		}
+		if(wxGetApp().ShouldAbort()){
+			return true;
+		}
+		//Set the old attributes back
+		if(data.GetIgnoreRO()){
+			#ifdef __WXMSW__
+				SetFileAttributes(data.GetDest(), iAttributes); 
+			#endif
+		} 
+	
+		if(data.GetAttributes() == true){
+			#ifdef __WXMSW__
+				int filearrtibs = GetFileAttributes(data.GetSource());
+				SetFileAttributes(data.GetDest(),FILE_ATTRIBUTE_NORMAL);                       
+				SetFileAttributes(data.GetDest(),filearrtibs);
+			#endif
+		}
+		if(data.GetTimeStamps() && ShouldTimeStamp){
+			wxFileName from(data.GetSource());
+			wxFileName to(data.GetDest());
+			wxDateTime access, mod, created;
+			from.GetTimes(&access ,&mod ,&created );
+			to.SetTimes(&access ,&mod , &created); 
+		}			
+	}
+	return true;
+}
+
+
+bool DirectoryRemove(wxString strLocation){
+	if(wxGetApp().ShouldAbort()){
+		return true;
+	}
+	// if it's a possible root directory
+	if (strLocation.length() <= 3) {
+		wxLogError(_("Toucan tried to delete a root directory. This has been forbidden for your own safety"));
+		return false;
+	}
+	//Make sure that the correct ending is appended
+	if (strLocation[strLocation.length()-1] != wxFILE_SEP_PATH) {
+		strLocation += wxFILE_SEP_PATH;       
+	}
+	
+	wxDir* dir = new wxDir(strLocation);
+	wxString strFilename;
+	bool blDir = dir->GetFirst(&strFilename);
+	if(blDir){
+		do {
+			if(wxGetApp().ShouldAbort()){
+				return true;
+			}
+			if(wxDirExists(strLocation + strFilename)){
+				DirectoryRemove(strLocation + strFilename);
+			}
+			else{
+				if(wxRemoveFile(strLocation + strFilename)){
+                    OutputProgress(_("Removed ") + strLocation + strFilename);
+                }
+            }
+	
+		}
+		while (dir->GetNext(&strFilename) );
+	} 
+	delete dir;
+  	wxLogNull log;
+	if(wxFileName::Rmdir(strLocation)){
+        OutputProgress(_("Removed ") + strLocation);
+    }
+	return true;
+}
+
+
+bool FolderTimeLoop(wxString strFrom, wxString strTo){
+	if(wxGetApp().ShouldAbort()){
+		return false;
+	}
+	//Make sure that the correct ending is appended
+	if (strTo[strTo.length()-1] != wxFILE_SEP_PATH) {
+		strTo += wxFILE_SEP_PATH;       
+	}
+	// for both dirs
+	if (strFrom[strFrom.length()-1] != wxFILE_SEP_PATH) {
+		strFrom += wxFILE_SEP_PATH;       
+	}
+	
+	wxDir* dir = new wxDir(strFrom);
+	wxString strFilename;
+	bool blDir = dir->GetFirst(&strFilename);
+	if(blDir){
+		do {
+			if(wxGetApp().ShouldAbort()){
+				return false;	
+			}
+			if(wxDirExists(strTo + strFilename)){
+				FolderTimeLoop(strFrom + strFilename, strTo + strFilename);
+			}
+		}
+		while (dir->GetNext(&strFilename) );
+	} 
+	delete dir;
+	if (wxDirExists(strTo)){
+		#ifdef __WXMSW__
+			//Need to tidy up this code and submit as a patch to wxWidgets
+			FILETIME ftCreated,ftAccessed,ftModified;
+			HANDLE hFrom = CreateFile(strFrom, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+			if(hFrom == INVALID_HANDLE_VALUE){
+			  return false;
+			}  
+			
+			GetFileTime(hFrom,&ftCreated,&ftAccessed,&ftModified);
+			CloseHandle(hFrom);
+			HANDLE hTo = CreateFile(strTo, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+			if(hTo == INVALID_HANDLE_VALUE){
+			  return false;
+			}  
+			SetFileTime(hTo,&ftCreated,&ftAccessed,&ftModified);
+			CloseHandle(hTo);
+		#elif defined(__WXGTK__)
+			wxFileName from(strFrom);
+			wxFileName to(strTo);
+			wxDateTime access, mod, created;
+			from.GetTimes(&access ,&mod ,&created );
+			to.SetTimes(&access ,&mod , &created); 
+		#endif
+		OutputProgress(_("Set folder timestamps for ") + strTo);
+	}	
+	return true;
+}
