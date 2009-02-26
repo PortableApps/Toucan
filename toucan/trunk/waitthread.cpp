0a8339c0dadb7617adfc01b2f83d658707d70438
Steven Lamerton <steve.lamerton@gmail.com>
2009-02-26 21:05:14 +0000

    [svn r393] Readd old process checking code

--- toucan/trunk/waitthread.cpp
+++ toucan/trunk/waitthread.cpp
@@ -7,8 +7,18 @@
 #include "waitthread.h"
 
 void *WaitThread::Entry(){
-	while(wxProcess::Exists(m_PID)){
-		m_Process->HasInput();
-	}
+	#ifdef __WXMSW__
+		HANDLE hProcess=OpenProcess(PROCESS_QUERY_INFORMATION,FALSE,m_PID);
+		DWORD lgReturn;
+		GetExitCodeProcess(hProcess, &lgReturn);
+		while(lgReturn == STILL_ACTIVE){
+			m_Process->HasInput();
+			GetExitCodeProcess(hProcess, &lgReturn);
+		}
+	#else
+		while(wxProcess::Exists(m_PID)){
+			m_Process->HasInput();
+		}
+	#endif
 	return NULL;
 }
