e13752a794539d8a6c7316126744ea291e1aed7a
Steven Lamerton <steve.lamerton@gmail.com>
2009-02-27 16:28:00 +0000

    [svn r395] Fix backup exclusion bug

--- toucan/trunk/backupdata.cpp
+++ toucan/trunk/backupdata.cpp
@@ -226,7 +226,7 @@ bool BackupData::CreateList(wxTextFile *file, Rules rules, wxString strPath, int
 				{
 					//Always call the function again to ensure that ALL files and folders are processed
 					CreateList(file, rules, strPath + strFilename, iRootLength);
-					if(!rules.ShouldExclude(strPath + strFilename, true)){
+					/*if(!rules.ShouldExclude(strPath + strFilename, true)){
 						if(iRootLength == 0){
 							wxString strCombined = strPath + strFilename;
 							strCombined = strCombined.Right(strCombined.Length() - iRootLength - 3);
@@ -237,7 +237,7 @@ bool BackupData::CreateList(wxTextFile *file, Rules rules, wxString strPath, int
 							strCombined = strCombined.Right(strCombined.Length() - iRootLength - 1);
 							file->AddLine(strCombined);
 						}
-					}
+					}*/
 				}
 				//If it is a file
 				else{
