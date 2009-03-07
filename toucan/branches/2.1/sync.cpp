6e00fa6fc2a09c49cf7d08a07c541cabbd593679
Steven Lamerton <steve.lamerton@gmail.com>
2009-03-07 13:26:06 +0000

    [svn r416] Implemented FolderContentsToVector

--- toucan/branches/2.1/sync.cpp
+++ toucan/branches/2.1/sync.cpp
@@ -7,16 +7,33 @@
 #include "toucan.h"
 #include "sync.h"
 #include "forms/frmprogress.h"
+#include <wx/dir.h>
 
 void *SyncThread::Entry(){
-
+	std::vector<wxString> source = FolderContentsToVector(m_Data.GetSource());
+	std::vector<wxString> destination = FolderContentsToVector(m_Data.GetDest());
 	wxCommandEvent event(wxEVT_COMMAND_BUTTON_CLICKED, ID_SCRIPTFINISH);
 	wxPostEvent(wxGetApp().ProgressWindow, event);
 	return NULL;
 }
 
 std::vector<wxString> SyncThread::FolderContentsToVector(wxString path){
-	
+	std::vector<wxString> paths;
+	//Tidy up the path the make sure it is a directory
+	if (path[path.length()-1] != wxFILE_SEP_PATH) {
+		path += wxFILE_SEP_PATH;       
+	}
+	if(wxDirExists(path)){
+		wxString filename;
+		wxDir dir(path);
+		if(dir.GetFirst(&filename)){
+			do{
+				paths.push_back(filename);
+			}
+			while(dir.GetNext(&filename));
+		}
+	}
+	return paths;
 }
 
 std::map<wxString, location> SyncThread::MergeVectorsToMap(std::vector<wxString> sourcevec, std::vector<wxString> destinationvec){
