6d7305450255228da995216485efdde9b4fa137b
Steven Lamerton <steve.lamerton@gmail.com>
2009-04-29 21:40:51 +0100

    Refactor variables code

--- toucan/trunk/variables.cpp
+++ toucan/trunk/variables.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////////
 // Author:      Steven Lamerton
-// Copyright:   Copyright (C) 2006-2007 Steven Lamerton
+// Copyright:   Copyright (C) 2006-2009 Steven Lamerton
 // Licence:     GNU GPL 2 (See readme for more info
 /////////////////////////////////////////////////////////////////////////////////
 
@@ -21,79 +21,66 @@ wxString Normalise(wxString strFilePath){
 	wxStringTokenizer tkz(strFilePath, wxT("@"), wxTOKEN_STRTOK);
 	while(tkz.HasMoreTokens()){
         token = tkz.GetNextToken();
-		if(token.Left(1) != wxT("\\") && token.Right(1) != wxT("\\")){
-			wxString strValue;
-			if(token == wxT("date")){
-				token = now.FormatISODate();
-				strReturn += token;
-			}
-			else if(token == wxT("time")){
-				token = now.Format(wxT("%H")) + wxT("-") +  now.Format(wxT("%M"));
-				strReturn += token;       
-			}
-			else if(token == wxT("YYYY")){
-				token = now.Format(wxT("%Y"));
-				strReturn += token;       
-			}
-			else if(token == wxT("MM")){
-				token = now.Format(wxT("%m"));
-				strReturn += token;       
-			}
-			else if(token == wxT("DD")){
-				token = now.Format(wxT("%d"));
-				strReturn += token;       
-			}
-			else if(token == wxT("hh")){
-				token = now.Format(wxT("%H"));
-				strReturn += token;       
-			}
-			else if(token == wxT("mm")){
-				token = now.Format(wxT("%M"));
-				strReturn += token;       
-			}
-			else if(token == wxT("drive")){
-				strReturn += wxPathOnly(wxStandardPaths::Get().GetExecutablePath()).Left(2);
-			}
-			else if(token == wxT("docs")){	
-				strReturn += wxStandardPaths::Get().GetDocumentsDir();
-			}
-			else if(token == wxT("volume")){
-				#ifdef __WXMSW__
-					wxString strName = wxEmptyString;
-					WCHAR volumeLabel[256]; 
-					GetVolumeInformation(wxGetApp().GetSettingsPath().Left(3), volumeLabel, sizeof(volumeLabel), NULL, 0, NULL, NULL, 0);
-					strName.Printf(wxT("%s"),volumeLabel); 
-					strReturn += strName;
-				#endif
-			}
-			else if(token == wxT("label")){
-			 	wxFileConfig* autorun = new wxFileConfig(wxT(""), wxT(""), wxGetApp().GetSettingsPath().Left(3) + wxFILE_SEP_PATH + wxT("autorun.inf"));
-				wxString label = autorun->Read(wxT("Autorun/Label"));
-				strReturn += label;
-				delete autorun;
-			}
-			else if(wxGetEnv(token , &strValue)){
-				strReturn += strValue;
-			}
-			else{
-				wxString strRead;
-				//If there is no value in the field with the computers name, or it is empty, then read the value in the 'other' field
-				//Also a quick check at the start to make sure there are actually groups in the file or it seems to multilate the whole config object
-				if(wxGetApp().m_Variables_Config->GetNumberOfGroups() != 0 && (wxGetApp().m_Variables_Config->Read(token + wxT("/") + wxGetFullHostName(), & strRead) == false || wxGetApp().m_Variables_Config->Read(token + wxT("/") + wxGetFullHostName()) == wxEmptyString))
-				{			
-					//If there is no value in the 'other' field then simply add the name of that section as it must me a folder name	
-					if(wxGetApp().m_Variables_Config->Read(token + wxT("/") + _("Other"), & strRead) == false){
-						strReturn = strReturn + token;                 
-					}
-					else{
-						strReturn += strRead;
-					}		
-				}
-				else{
-					strReturn += token;
-				}
-			}
+		wxString strValue, read;
+		if(token == wxT("date")){
+			token = now.FormatISODate();
+			strReturn += token;
 		}
+		else if(token == wxT("time")){
+			token = now.Format(wxT("%H")) + wxT("-") +  now.Format(wxT("%M"));
+			strReturn += token;       
+		}
+		else if(token == wxT("YYYY")){
+			token = now.Format(wxT("%Y"));
+			strReturn += token;       
+		}
+		else if(token == wxT("MM")){
+			token = now.Format(wxT("%m"));
+			strReturn += token;       
+		}
+		else if(token == wxT("DD")){
+			token = now.Format(wxT("%d"));
+			strReturn += token;       
+		}
+		else if(token == wxT("hh")){
+			token = now.Format(wxT("%H"));
+			strReturn += token;       
+		}
+		else if(token == wxT("mm")){
+			token = now.Format(wxT("%M"));
+			strReturn += token;       
+		}
+		else if(token == wxT("drive")){
+			strReturn += wxPathOnly(wxStandardPaths::Get().GetExecutablePath()).Left(2);
+		}
+		else if(token == wxT("docs")){	
+			strReturn += wxStandardPaths::Get().GetDocumentsDir();
+		}
+		else if(token == wxT("volume")){
+			#ifdef __WXMSW__
+				wxString strName = wxEmptyString;
+				WCHAR volumeLabel[256]; 
+				GetVolumeInformation(wxGetApp().GetSettingsPath().Left(3), volumeLabel, sizeof(volumeLabel), NULL, 0, NULL, NULL, 0);
+				strName.Printf(wxT("%s"),volumeLabel); 
+				strReturn += strName;
+			#endif
+		}
+		else if(token == wxT("label")){
+		 	wxFileConfig* autorun = new wxFileConfig(wxT(""), wxT(""), wxGetApp().GetSettingsPath().Left(3) + wxFILE_SEP_PATH + wxT("autorun.inf"));
+			wxString label = autorun->Read(wxT("Autorun/Label"));
+			strReturn += label;
+			delete autorun;
+		}
+		else if(wxGetEnv(token , &strValue)){
+			strReturn += strValue;
+		}
+		else if(wxGetApp().m_Variables_Config->Read(token + wxT("/") + wxGetFullHostName(), &read) != false && wxGetApp().m_Variables_Config->GetNumberOfGroups() != 0){
+			strReturn += read;		
+		}
+		else if(wxGetApp().m_Variables_Config->Read(token + wxT("/") + _("Other"), &read) != false && wxGetApp().m_Variables_Config->GetNumberOfGroups() != 0){
+			strReturn += read;		
+		}
+		//Otherwise we dont have a match, just append the token
 		else{
 			strReturn += token;
 		}
