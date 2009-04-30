299e17b2153fe3ddf019b50418f8b25049ddc857
Steven Lamerton <steve.lamerton@gmail.com>
2009-04-30 17:21:52 +0100

    Fix for @ signs in paths

--- toucan/trunk/variables.cpp
+++ toucan/trunk/variables.cpp
@@ -19,42 +19,52 @@ wxString Normalise(wxString strFilePath){
 	wxString strReturn = wxEmptyString;
 	wxDateTime now = wxDateTime::Now();  
 	wxStringTokenizer tkz(strFilePath, wxT("@"), wxTOKEN_STRTOK);
+	bool previousmatched = true;
 	while(tkz.HasMoreTokens()){
         token = tkz.GetNextToken();
 		wxString strValue, read;
 		if(token == wxT("date")){
 			token = now.FormatISODate();
 			strReturn += token;
+			previousmatched = true;
 		}
 		else if(token == wxT("time")){
 			token = now.Format(wxT("%H")) + wxT("-") +  now.Format(wxT("%M"));
-			strReturn += token;       
+			strReturn += token;
+			previousmatched = true;
 		}
 		else if(token == wxT("YYYY")){
 			token = now.Format(wxT("%Y"));
-			strReturn += token;       
+			strReturn += token;
+			previousmatched = true;
 		}
 		else if(token == wxT("MM")){
 			token = now.Format(wxT("%m"));
-			strReturn += token;       
+			strReturn += token;
+			previousmatched = true;
 		}
 		else if(token == wxT("DD")){
 			token = now.Format(wxT("%d"));
-			strReturn += token;       
+			strReturn += token;
+			previousmatched = true;
 		}
 		else if(token == wxT("hh")){
 			token = now.Format(wxT("%H"));
-			strReturn += token;       
+			strReturn += token;
+			previousmatched = true;
 		}
 		else if(token == wxT("mm")){
 			token = now.Format(wxT("%M"));
-			strReturn += token;       
+			strReturn += token;
+			previousmatched = true;
 		}
 		else if(token == wxT("drive")){
 			strReturn += wxPathOnly(wxStandardPaths::Get().GetExecutablePath()).Left(2);
+			previousmatched = true;
 		}
-		else if(token == wxT("docs")){	
+		else if(token == wxT("docs")){
 			strReturn += wxStandardPaths::Get().GetDocumentsDir();
+			previousmatched = true;
 		}
 		else if(token == wxT("volume")){
 			#ifdef __WXMSW__
@@ -64,25 +74,36 @@ wxString Normalise(wxString strFilePath){
 				strName.Printf(wxT("%s"),volumeLabel); 
 				strReturn += strName;
 			#endif
+			previousmatched = true;
 		}
 		else if(token == wxT("label")){
 		 	wxFileConfig* autorun = new wxFileConfig(wxT(""), wxT(""), wxGetApp().GetSettingsPath().Left(3) + wxFILE_SEP_PATH + wxT("autorun.inf"));
 			wxString label = autorun->Read(wxT("Autorun/Label"));
 			strReturn += label;
 			delete autorun;
+			previousmatched = true;
 		}
 		else if(wxGetEnv(token , &strValue)){
 			strReturn += strValue;
+			previousmatched = true;
 		}
 		else if(wxGetApp().m_Variables_Config->Read(token + wxT("/") + wxGetFullHostName(), &read) != false && wxGetApp().m_Variables_Config->GetNumberOfGroups() != 0){
-			strReturn += read;		
+			strReturn += read;
+			previousmatched = true;
 		}
 		else if(wxGetApp().m_Variables_Config->Read(token + wxT("/") + _("Other"), &read) != false && wxGetApp().m_Variables_Config->GetNumberOfGroups() != 0){
-			strReturn += read;		
+			strReturn += read;
+			previousmatched = true;
 		}
-		//Otherwise we dont have a match, just append the token
 		else{
-			strReturn += token;
+			if(previousmatched){
+				strReturn += token;
+			}
+			else{
+				strReturn = strReturn + wxT("@") + token;
+			}
+			//This time we did not match
+			previousmatched = false;
 		}
 	}
 
