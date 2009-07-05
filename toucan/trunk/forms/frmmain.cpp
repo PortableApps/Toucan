8ce5a667a389f3f066ab38b2775f6efb069e73a5
Steven Lamerton <steve.lamerton@gmail.com>
2009-07-05 14:36:47 +0100

    Version bump

--- toucan/trunk/forms/frmmain.cpp
+++ toucan/trunk/forms/frmmain.cpp
@@ -1658,7 +1658,7 @@ void frmMain::OnSecureAddVarClick(wxCommandEvent& WXUNUSED(event)){
 void frmMain::OnAboutClick(wxCommandEvent& WXUNUSED(event)){
 	wxAboutDialogInfo info;
 	info.SetName(wxT("Toucan"));
-	info.SetVersion(wxT("2.1.1 Pre-Release 2"));
+	info.SetVersion(wxT("2.1.1"));
 	info.SetCopyright(wxT("(C) 2006-2009 Steven Lamerton \nName by Danny Mensingh\nMain icons by Neorame\nOther icons by Silvestre Herrera\nExtra thanks to Jorgen Bodde for his awesome wxVirtualDirTreeCtrl\n7Zip and ccrypt are by their respective teams.\nAll items (C) their owners."));
 	info.SetWebSite(wxT("http://portableapps.com/toucan"));
 	info.SetLicense(wxT("Toucan and its component parts are all licensed under the GNU GPL Version 2 or a compatible license."));
