31219a46eb90a4a5d34a95fc217991f1ebaa4e2d
Steven Lamerton <steve.lamerton@gmail.com>
2009-07-14 17:26:44 +0100

    Add build time and date to the about box

--- toucan/trunk/forms/frmmain.cpp
+++ toucan/trunk/forms/frmmain.cpp
@@ -1648,6 +1648,7 @@ void frmMain::OnAboutClick(wxCommandEvent& WXUNUSED(event)){
 	wxAboutDialogInfo info;
 	info.SetName(wxT("Toucan"));
 	info.SetVersion(wxT("2.1.2"));
+	info.SetDescription(wxString::Format(wxT("Built on %s at %s"), __TDATE__, __TTIME__));
 	info.SetCopyright(wxT("(C) 2006-2009 Steven Lamerton \nName by Danny Mensingh\nMain icons by Neorame\nOther icons by Silvestre Herrera\nExtra thanks to Jorgen Bodde for his awesome wxVirtualDirTreeCtrl\n7Zip and ccrypt are by their respective teams.\nAll items (C) their owners."));
 	info.SetWebSite(wxT("http://portableapps.com/toucan"));
 	info.SetLicense(wxT("Toucan and its component parts are all licensed under the GNU GPL Version 2 or a compatible license."));
