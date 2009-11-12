96fae18255e1ae8fcd3f421c67cf5120bf03c6a9
Steven Lamerton <steve.lamerton@gmail.com>
2009-11-12 16:20:43 +0000

    Initial add of ccrypt code

---
+++ toucan/trunk/ccrypt/tables.h
@@ -0,0 +1,10 @@
+/* Copyright (C) 2000-2009 Peter Selinger.
+   This file is part of ccrypt. It is free software and it is covered
+   by the GNU general public license. See the file COPYING for details. */
+
+extern word8x4 M0[4][256];
+extern word8x4 M1[4][256];
+extern int xrcon[30];
+extern word8 xS[256];
+extern word8 xSi[256];
+
