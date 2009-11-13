dae0dce64e98b61f98d44a320de4e63f0dd018e4
Steven Lamerton <steve.lamerton@gmail.com>
2009-11-13 22:46:19 +0000

    Move lua name bindings to file

--- toucan/trunk/luathread.cpp
+++ toucan/trunk/luathread.cpp
@@ -5,6 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 #include <wx/listctrl.h>
+#include <wx/stdpaths.h>
 
 #include "toucan.h"
 #include "luathread.h"
@@ -19,13 +20,10 @@ LuaThread::LuaThread(const wxString &line) : wxThread(), m_Command(line){
 	m_State = luaL_newstate();
 	luaL_openlibs(m_State);
 	luaopen_toucan(m_State);
-	//TODO : Move this to a bindings file
-	wxString setprint = wxT("print=toucan.OutputProgress");
-	luaL_dostring(m_State, setprint.mb_str());
-	wxString setsync  = wxT("sync=toucan.Sync"); 
-	luaL_dostring(m_State, setsync.mb_str());
-	wxString setexpand = wxT("expand=toucan.ExpandVariable");
-	luaL_dostring(m_State, setexpand.mb_str());
+	wxString path = wxPathOnly(wxStandardPaths::Get().GetExecutablePath()) + wxFILE_SEP_PATH + wxT("bindings.lua");
+	if (luaL_loadfile(m_State, path.mb_str()) || lua_pcall(m_State, 0, 0, 0)) {
+		OutputProgress(wxT("Cannot run lua file: ") + wxString(lua_tostring(m_State, -1), wxConvUTF8));
+	}
 }
 
 void *LuaThread::Entry(){
