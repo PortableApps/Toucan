761a106ab40c426139f48e3ae080014330b51b2d
Steven Lamerton <steve.lamerton@gmail.com>
2009-11-14 22:54:15 +0000

    Add new keywords to syntax highlighter

--- toucan/trunk/forms/frmmain.cpp
+++ toucan/trunk/forms/frmmain.cpp
@@ -707,7 +707,7 @@ void frmMain::CreateControls(){
 	//Lua keywords in 0
 	m_Script_Styled->SetKeyWords(0, wxT("and break do else elseif end false for function if in local nil not or repeat return then true until while"));
 	//Toucan keywords in 1
-	m_Script_Styled->SetKeyWords(1, wxT("print sync backup secure expand"));
+	m_Script_Styled->SetKeyWords(1, wxT("sync backup secure print expand copy rename move delete execute"));
 	m_Script_Styled->SetWrapMode(1);
 	m_Script_Styled->SetLexer(wxSTC_LEX_LUA);
 	ScriptSizer->Add(m_Script_Styled, 1, wxEXPAND|wxLEFT|wxRIGHT|wxBOTTOM, 2 * border);
