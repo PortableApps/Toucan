07facec236228f41ef88afaf48e3b1bfbf3a761a
Steven Lamerton <steve.lamerton@gmail.com>
2009-11-21 16:06:17 +0000

    Remove extra directory timestamp code as it is included in wxwidgets 2.9

--- sync/syncjob.cpp
+++ sync/syncjob.cpp
@@ -360,29 +360,11 @@ bool SyncFiles::RemoveDirectory(wxString path){
 }
 
 bool SyncFiles::CopyFolderTimestamp(const wxString &source, const wxString &dest){
-	#ifdef __WXMSW__
-		//Need to tidy up this code and submit as a patch to wxWidgets
-		FILETIME ftCreated, ftAccessed, ftModified;
-		HANDLE hFrom = CreateFile(source, GENERIC_READ, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
-		if(hFrom == INVALID_HANDLE_VALUE){
-		  return false;
-		}
-		GetFileTime(hFrom, &ftCreated, &ftAccessed, &ftModified);
-		CloseHandle(hFrom);
-
-		HANDLE hTo = CreateFile(dest, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
-		if(hTo == INVALID_HANDLE_VALUE){
-		  return false;
-		}  
-		SetFileTime(hTo, &ftCreated, &ftAccessed, &ftModified);
-		CloseHandle(hTo);
-	#else
-		wxFileName from(source);
-		wxFileName to(dest);
-		wxDateTime access, mod, created;
-		from.GetTimes(&access, &mod, &created);
-		to.SetTimes(&access, &mod, &created); 
-	#endif
+	wxFileName from(source);
+	wxFileName to(dest);
+	wxDateTime access, mod, created;
+	from.GetTimes(&access, &mod, &created);
+	to.SetTimes(&access, &mod, &created); 
 	return true;
 }
 
