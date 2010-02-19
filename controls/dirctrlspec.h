7d3e624ee1dcbcd94f5aec8600cd9f3ab35b5a38
Steven Lamerton <steve.lamerton@gmail.com>
2010-02-19 21:15:20 +0000

    Tidy constructor alignment

--- controls/dirctrlspec.h
+++ controls/dirctrlspec.h
@@ -10,8 +10,10 @@
 class LocalDirCtrl : public DirCtrl{
 
 public:
-	LocalDirCtrl(wxWindow* parent, wxWindowID id, const wxPoint& pos = wxDefaultPosition,
-	                     const wxSize& size = wxDefaultSize, long style = wxTR_HAS_BUTTONS|wxTR_MULTIPLE|wxTR_HIDE_ROOT);
+	LocalDirCtrl(wxWindow* parent, wxWindowID id,
+					const wxPoint& pos = wxDefaultPosition,
+					const wxSize& size = wxDefaultSize,
+					long style = wxTR_HAS_BUTTONS|wxTR_MULTIPLE|wxTR_HIDE_ROOT);
 
 	void ReCreateTree();
 };
