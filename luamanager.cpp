60a2fcf4c6c9e24f1de9bcf1286f65b58eab6833
Steven Lamerton <steve.lamerton@gmail.com>
2010-04-14 19:22:18 +0100

    Add extra output at the start of job, small refactoring

--- luamanager.cpp
+++ luamanager.cpp
@@ -1,9 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////////
 // Author:      Steven Lamerton
-// Copyright:   Copyright (C) 2009 Steven Lamerton
-// License:     GNU GPL 2 (See readme for more info)
+// Copyright:   Copyright (C) 2009 - 2010 Steven Lamerton
+// License:     GNU GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
 /////////////////////////////////////////////////////////////////////////////////
-
 #include <wx/button.h>
 #include <wx/listctrl.h>
 
@@ -26,63 +25,62 @@ LuaManager::LuaManager(){
 
 void LuaManager::StartUp(){
 	m_StartTime = wxDateTime::Now();
-	if(!wxGetApp().IsGui()){
-		OutputProgress(_("Starting"), true);
-		OutputProgress(wxEmptyString);
-		return;
+	if(wxGetApp().IsGui()){
+		if(!m_Window){
+			m_Window = new frmProgress(wxGetApp().MainWindow, wxID_ANY, _("Progress"), wxDefaultPosition, wxSize(640, 480), wxDEFAULT_FRAME_STYLE|wxRESIZE_BORDER);
+			m_Window->Show();
+		}
+		else{
+			m_Window->m_List->DeleteAllItems();
+		}
+		LogListCtrl* logList = new LogListCtrl(m_Window->m_List);
+		delete wxLog::SetActiveTarget(logList);
 	}
-	if(!m_Window){
-		m_Window = new frmProgress(wxGetApp().MainWindow, wxID_ANY, _("Progress"), wxDefaultPosition, wxSize(640, 480), wxDEFAULT_FRAME_STYLE|wxRESIZE_BORDER);
-		m_Window->Show();
-	}
-	else{
-		m_Window->m_List->DeleteAllItems();
-	}
-	//Send all errors to the list control
-	LogListCtrl* logList = new LogListCtrl(m_Window->m_List);
-	delete wxLog::SetActiveTarget(logList);
-	OutputProgress(_("Starting"), true);
+	OutputProgress("Toucan 3.0 Pre-Release 1", true);
+	OutputProgress(_("Date:") + " " + m_StartTime.FormatISODate());
+	OutputProgress(_("Computer Name:") + " " + wxGetHostName());
+	OutputProgress(_("Operating System:") + " " + wxGetOsDescription());
 	OutputProgress(wxEmptyString);
-	m_Window->m_List->SetColumnWidth(1, m_Window->m_List->GetClientSize().GetWidth() - m_Window->m_List->GetColumnWidth(0));
-	m_Window->m_OK->Enable(false);
-	m_Window->m_Save->Enable(false);
-	m_Window->m_Cancel->Enable(true);
+	if(wxGetApp().IsGui()){
+		m_Window->m_List->SetColumnWidth(1, m_Window->m_List->GetClientSize().GetWidth() - m_Window->m_List->GetColumnWidth(0));
+		m_Window->m_OK->Enable(false);
+		m_Window->m_Save->Enable(false);
+		m_Window->m_Cancel->Enable(true);
+	}
 }
 
 void LuaManager::CleanUp(){
-	if(!wxGetApp().IsGui()){
+	if(wxGetApp().IsGui()){
+		//Send all errors to the standard gui
+		delete wxLog::SetActiveTarget(new wxLogGui);
 		OutputProgress(wxEmptyString);
-		OutputProgress(_("Elapsed") + wxT("    ") + wxDateTime::Now().Subtract(m_StartTime).Format());
-		OutputProgress(_("Finished"), true);
-		return;
 	}
-	//Send all errors to the standard gui
-	delete wxLog::SetActiveTarget(new wxLogGui);
-	OutputProgress(wxEmptyString);
-	OutputProgress(_("Elapsed") + wxT(" ") + wxDateTime::Now().Subtract(m_StartTime).Format());
+	OutputProgress(_("Elapsed:") + wxT(" ") + wxDateTime::Now().Subtract(m_StartTime).Format());
 	OutputProgress(_("Finished"), true);
-	m_Window->m_OK->Enable(true);
-	m_Window->m_Save->Enable(true);
-	m_Window->m_Cancel->Enable(false);
+	if(wxGetApp().IsGui()){
+		m_Window->m_OK->Enable(true);
+		m_Window->m_Save->Enable(true);
+		m_Window->m_Cancel->Enable(false);
 
-	//Let the user know we have finished
-	m_Window->FinishGauge();
+		//Let the user know we have finished
+		m_Window->FinishGauge();
 #ifdef __WXMSW__
-	FLASHWINFO info;
-	info.uCount = 3;
-	info.dwTimeout = 0;
-	info.hwnd = static_cast<HWND>(m_Window->GetHWND());
-	info.cbSize = sizeof(info);
-	if(wxWindow::FindFocus() == m_Window){
-		info.dwFlags = FLASHW_ALL|FLASHW_TIMERNOFG;			
-	}
-	else{
-		info.dwFlags = FLASHW_ALL;					
-	}
-	FlashWindowEx(&info);
+		FLASHWINFO info;
+		info.uCount = 3;
+		info.dwTimeout = 0;
+		info.hwnd = static_cast<HWND>(m_Window->GetHWND());
+		info.cbSize = sizeof(info);
+		if(wxWindow::FindFocus() == m_Window){
+			info.dwFlags = FLASHW_ALL|FLASHW_TIMERNOFG;			
+		}
+		else{
+			info.dwFlags = FLASHW_ALL;					
+		}
+		FlashWindowEx(&info);
 #else
-	m_Window->RequestUserAttention();
+		m_Window->RequestUserAttention();
 #endif
+	}
 }
 
 void LuaManager::Run(const wxString &line){
