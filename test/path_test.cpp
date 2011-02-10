47223ea1617854935eed5d24524d8109570edd0d
Steven Lamerton <steve.lamerton@gmail.com>
2011-02-10 14:31:33 +0000

    Tidy the path expansion unit tests

--- test/path_test.cpp
+++ test/path_test.cpp
@@ -8,50 +8,56 @@
 #include "../path.h"
 
 TEST(PathNormalise, Date){
-    EXPECT_EQ(Path::Normalise(wxT("@date@")), wxDateTime::Now().FormatISODate());
+    EXPECT_EQ(Path::Normalise("@date@"), wxDateTime::Now().FormatISODate());
 }
 
 TEST(PathNormalise, Time){
-    EXPECT_EQ(Path::Normalise(wxT("@time@")), wxDateTime::Now().Format(wxT("%H")) + wxT("-") +  wxDateTime::Now().Format(wxT("%M")));
+    EXPECT_EQ(Path::Normalise("@time@"), wxDateTime::Now().Format("%H") + "-" +  wxDateTime::Now().Format("%M"));
 }
 
 TEST(PathNormalise, Year){
-    EXPECT_EQ(Path::Normalise(wxT("@YYYY@")), wxDateTime::Now().Format(wxT("%Y")));
+    EXPECT_EQ(Path::Normalise("@year@"), wxDateTime::Now().Format("%Y"));
+    EXPECT_EQ(Path::Normalise("@YYYY@"), wxDateTime::Now().Format("%Y"));
 }
 
 TEST(PathNormalise, Month){
-    EXPECT_EQ(Path::Normalise(wxT("@MM@")), wxDateTime::Now().Format(wxT("%m")));
+    EXPECT_EQ(Path::Normalise("@month@"), wxDateTime::Now().Format("%m"));
+    EXPECT_EQ(Path::Normalise("@MM@"), wxDateTime::Now().Format("%m"));
 }
 
 TEST(PathNormalise, Day){
-    EXPECT_EQ(Path::Normalise(wxT("@DD@")), wxDateTime::Now().Format(wxT("%d")));
+    EXPECT_EQ(Path::Normalise("@day@"), wxDateTime::Now().Format("%d"));
+    EXPECT_EQ(Path::Normalise("@DD@"), wxDateTime::Now().Format("%d"));
 }
 
 TEST(PathNormalise, Hour){
-    EXPECT_EQ(Path::Normalise(wxT("@hh@")), wxDateTime::Now().Format(wxT("%H")));
+    EXPECT_EQ(Path::Normalise("@hour@"), wxDateTime::Now().Format("%H"));
+    EXPECT_EQ(Path::Normalise("@hh@"), wxDateTime::Now().Format("%H"));
 }
 
 TEST(PathNormalise, Minute){
-    EXPECT_EQ(Path::Normalise(wxT("@mm@")), wxDateTime::Now().Format(wxT("%M")));
+    EXPECT_EQ(Path::Normalise("@minute@"), wxDateTime::Now().Format("%M"));
+    EXPECT_EQ(Path::Normalise("@mm@"), wxDateTime::Now().Format("%M"));
 }
 
-TEST(PathNormalise, EmbeddedAt){
-    EXPECT_EQ(Path::Normalise(wxT("C:\\thisisa@test\\path.doc")), wxT("C:\\thisisa@test\\path.doc"));
+TEST(PathNormalise, MismatchedAt){
+    EXPECT_EQ(Path::Normalise("C:\\thisisa@test\\path.doc"), "C:\\thisisa@test\\path.doc");
+    EXPECT_EQ(Path::Normalise("C:\\testdir\\@@date@.zip"), "C:\\testdir\\@" + wxDateTime::Now().FormatISODate() + ".zip");
+    EXPECT_EQ(Path::Normalise("C:\\testdir\\@@date@@.zip"), "C:\\testdir\\@" + wxDateTime::Now().FormatISODate() + "@.zip");
 }
 
 TEST(PathNormalise, DateEmbedded){
-    EXPECT_EQ(Path::Normalise(wxT("C:\\testdir\\@date@.zip")), wxT("C:\\testdir\\") + wxDateTime::Now().FormatISODate() + wxT(".zip"));
+    EXPECT_EQ(Path::Normalise("C:\\testdir\\@date@.zip"),"C:\\testdir\\" + wxDateTime::Now().FormatISODate() + ".zip");
 }
 
 TEST(PathNormalise, BunchedEmbeddded){
-    EXPECT_EQ(Path::Normalise(wxT("C:\\@YYYY@@MM@\\@date@.zip")), wxT("C:\\") + wxDateTime::Now().Format(wxT("%Y")) + wxDateTime::Now().Format(wxT("%m")) + wxT("\\") + wxDateTime::Now().FormatISODate()+ wxT(".zip"));
-}
-
-TEST(PathNormalise, ExtraAt){
-    EXPECT_EQ(Path::Normalise(wxT("C:\\testdir\\@@date@.zip")), wxT("C:\\testdir\\@") + wxDateTime::Now().FormatISODate() + wxT(".zip"));
-    EXPECT_EQ(Path::Normalise(wxT("C:\\testdir\\@@date@@.zip")), wxT("C:\\testdir\\@") + wxDateTime::Now().FormatISODate() + wxT("@.zip"));
+    EXPECT_EQ(Path::Normalise("C:\\@YYYY@@MM@\\@date@.zip"), "C:\\" + wxDateTime::Now().Format("%Y") + wxDateTime::Now().Format("%m") + "\\" + wxDateTime::Now().FormatISODate() + ".zip");
 }
 
 TEST(PathNormalise, Plain){
-    EXPECT_EQ(Path::Normalise(wxT("docs")), wxT("docs"));
+    EXPECT_EQ(Path::Normalise("docs"), "docs");
+}
+
+TEST(PathNormalise, DoesntExist){
+    EXPECT_EQ(Path::Normalise("C:\\thisisa@test@\\path.doc"), "C:\\thisisa@test@\\path.doc");
 }
