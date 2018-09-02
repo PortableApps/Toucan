4ce9cb17df2b8bb2d054a481a851903c613f67fd
Alex Tereschenko <alttoucan@gmail.com>
2018-09-02 17:10:08 +0200

    docs: fix Sphinx warnings
    Signed-off-by: Alex Tereschenko <alttoucan@gmail.com>

--- help/source/conf.py
+++ help/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Toucan'
-copyright = '2013, Steve Lamerton, Alex Tereschenko'
+copyright = '2013-2018, Steve Lamerton, Alex Tereschenko'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -121,7 +121,7 @@ html_logo = 'toucan.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
