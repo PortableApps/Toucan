0baf33d50243a1f1cf5d6664b6045de13fe31711
Alex Tereschenko <alttoucan@gmail.com>
2019-01-05 20:12:28 +0100

    about, docs: update copyright year
    Signed-off-by: Alex Tereschenko <alttoucan@gmail.com>

--- help/source/conf.py
+++ help/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Toucan'
-copyright = '2013-2018, Steve Lamerton, Alex Tereschenko'
+copyright = '2013-2019, Steve Lamerton, Alex Tereschenko'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
